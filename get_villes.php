d965f17c776252d6b09fc2ea998c3339abc4b559
Chaimambk <161932110+Chaimambk@users.noreply.github.com>
2025-03-26 11:46:32 +0100

    Create get_villes.php

---
+++ get_villes.php
@@ -0,0 +1,11 @@
+<?php
+require 'database.php';
+
+$sql = "SELECT nom, latitude, longitude, description FROM villes";
+$stmt = $pdo->query($sql);
+$villes = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+header('Content-Type: application/json');
+echo json_encode($villes);
+?>
+
